1a7d3fc0a88c5248b04f32dd10c5a6a781c97113
Hendrik Schneider <835099+hendrikschneider@users.noreply.github.com>
2026-01-24 11:37:38 +0200

    Add post_migrate signal for topic population
    Connect post_migrate signal to populate topics after migration.

--- django_webhook/apps.py
+++ django_webhook/apps.py
@@ -1,6 +1,6 @@
 # pylint: disable=import-outside-toplevel
-
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class WebhooksConfig(AppConfig):
@@ -8,10 +8,20 @@ class WebhooksConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
 
     def ready(self):
-        # pylint: disable=unused-import
-        import django_webhook.checks
-        from django_webhook.models import populate_topics_from_settings
-        from django_webhook.signals import connect_signals
+        # --- SAFE: no DB access here --------------------
+        import django_webhook.checks  # noqa: F401
 
+        from django_webhook.signals import connect_signals
         connect_signals()
+
+        # --- DB-dependent logic: defer until DB exists ---
+        post_migrate.connect(
+            self._populate_topics,
+            sender=self,
+            dispatch_uid="django_webhook.populate_topics",
+        )
+
+    @staticmethod
+    def _populate_topics(**kwargs):
+        from django_webhook.models import populate_topics_from_settings
         populate_topics_from_settings()
